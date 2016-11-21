0e499c0ad1ea821753906d1abf31b3182677ef56
wushaojun321 <418836702@qq.com>
2016-11-21 22:39:16 +0800

    17

--- manage.py
+++ manage.py
@@ -16,5 +16,5 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-	# manager.run()
-	app.run(host='0.0.0.0',port=80,debug=True)
+	manager.run()
+	# app.run(host='0.0.0.0',port=80,debug=True)
